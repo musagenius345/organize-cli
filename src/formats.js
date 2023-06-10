aef5acffa39576877fed4b55c60288410d4098b7
Y Blackfrost <64871730+musagenius345@users.noreply.github.com>
2023-06-10 21:10:32 +0300

    Update formats.js

--- src/formats.js
+++ src/formats.js
@@ -1,10 +1,10 @@
-const Music = ['MP3', 'WAV', 'WMA', 'MKA', 'AAC', 'MID', 'RA', 'RAM', 'RM', 'OGG'];
-const Codes = ['CPP', 'RB', 'PY', 'HTML', 'CSS', 'JS', 'PHP'];
+const Music = ['MP3', 'MKV', 'WAV', 'WMA', 'MKA', 'AAC', 'MID', 'RA', 'RAM', 'RM', 'OGG'];
+const Codes = ['CPP', 'RB', 'PY', 'HTML', 'CSS', 'JS', 'PHP', 'SCSS'];
 const Compressed = ['RAR', 'JAR', 'ZIP', 'TAR', 'MAR', 'ISO', 'LZ', '7ZIP', 'TGZ', 'GZ', 'BZ2'];
 const Documents = ['DOC', 'DOCX', 'PPT', 'PPTX', 'PAGES', 'PDF', 'ODT', 'ODP', 'XLSX', 'XLS', 'ODS', 'TXT', 'IN', 'OUT', 'MD'];
 const Images = ['JPG', 'JPEG', 'GIF', 'PNG', 'SVG'];
 const Executables = ['DEB', 'EXE', 'SH', 'BUNDLE'];
-const Video = ['FLV', 'WMV', 'MOV', 'MP4', 'MPEG', '3GP', 'MKV'];
+const Video = ['FLV', 'WMV', 'WEBM', 'MOV', 'MP4', 'MPEG', '3GP', 'MKV'];
 
 module.exports = {
   Music,
